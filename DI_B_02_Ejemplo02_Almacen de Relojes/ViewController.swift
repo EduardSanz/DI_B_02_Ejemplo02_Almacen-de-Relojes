6ab1dadb8b9d6bda67d4c89c27c7953f723282ec
eduard <eduard@cieep.com>
2022-11-25 13:36:38 +0100

    Initial Commit

---
+++ DI_B_02_Ejemplo02_Almacen de Relojes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI_B_02_Ejemplo02_Almacen de Relojes
+//
+//  Created by Eduard on 25/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
